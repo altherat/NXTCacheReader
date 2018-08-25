4e4c7f1256e68ddd89f997a3186b1a987a0e1212
altherat <6601541+altherat@users.noreply.github.com>
2018-08-24 22:04:19 -0500

    Update CacheReader.cs

--- src/CacheReader.cs
+++ src/CacheReader.cs
@@ -2,9 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Data.SQLite;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.IO.Compression;
 using zlib;
 
 namespace NXTCacheReader
