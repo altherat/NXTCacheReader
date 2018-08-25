8c7c03e44b6024fdd6e5cbf80901bc6f9e891c64
altherat <6601541+altherat@users.noreply.github.com>
2018-08-24 22:03:04 -0500

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,10 @@
-using NXTCacheReader;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test
+namespace NXTCacheReader
 {
     class Program
     {
