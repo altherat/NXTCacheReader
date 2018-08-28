9da05d6d534a987f31b157c972d4b2914fcc046b
altherat <6601541+altherat@users.noreply.github.com>
2018-08-28 01:06:34 -0500

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -7,11 +7,13 @@ namespace NXTCacheReader
     class Program
     {
 
+        private static CacheReader cacheReader;
+
         static void Main(string[] args)
         {
 
             // Instantiate the cache reader
-            CacheReader cacheReader = new CacheReader(@"C:\ProgramData\Jagex\\RuneScape");
+            cacheReader = new CacheReader(@"C:\ProgramData\Jagex\\RuneScape");
 
             // Load the game object with ID 69860
             Definition.Object objectDefinition = cacheReader.LoadDefinition<Definition.Object>(69860);
@@ -33,7 +35,79 @@ namespace NXTCacheReader
             // Print the item's actions
             Console.WriteLine(String.Join(", ", bluePartyhat.Actions));
 
+            //Quick chat demo
+            Console.WriteLine("\nQuick chat demo:");
+            QuickChatDemo();
+
             Console.ReadLine();
+        }
+
+        private static void QuickChatDemo()
+        {
+            Dictionary<int, Definition.QuickChat> definitions = cacheReader.LoadDefinitions<Definition.QuickChat>();
+            int id = 85;
+            while (true)
+            {
+                Definition.QuickChat definition = definitions[id];
+                int[] subMenuIds = definition.SubMenuIds;
+                int[] subOptionIds = definition.SubOptionIds;
+                if (definition.IsMenu)
+                {
+                    int subMenuCount;
+                    int subOptionCount;
+                    if (subMenuIds == null)
+                    {
+                        subMenuCount = 0;
+                    }
+                    else
+                    {
+                        subMenuCount = subMenuIds.Length;
+                        for (int i = 0; i < subMenuCount; i++)
+                        {
+                            Console.WriteLine(i + ": " + definitions[subMenuIds[i]].Text + " ->");
+                        }
+                    }
+                    if (subOptionIds == null)
+                    {
+                        subOptionCount = 0;
+                    }
+                    else
+                    {
+                        subOptionCount = subOptionIds.Length;
+                        for (int i = 0; i < subOptionCount; i++)
+                        {
+                            Console.WriteLine((subMenuCount + i) + ": " + definitions[subOptionIds[i]].Text);
+                        }
+                    }
+                    Console.Write("\nSelect option: ");
+                    id = int.Parse(Console.ReadLine());
+                    if (id >= subMenuCount)
+                    {
+                        id = subOptionIds[id - subMenuCount];
+                    }
+                    else
+                    {
+                        id = subMenuIds[id];
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nSelected option: " + definition.Text);
+                    if (subOptionIds == null)
+                    {
+                        Console.WriteLine("Quick-reply options: None");
+                    }
+                    else {
+                        int quickReplyCount = subOptionIds.Length;
+                        Console.WriteLine("Quick-reply options: " + quickReplyCount);
+                        for (int i = 0; i < quickReplyCount; i++)
+                        {
+                            Console.WriteLine("\t" + i + ": " + definitions[subOptionIds[i]].Text);
+                        }
+                    }
+                    break;
+                }
+            }
         }
         
     }
